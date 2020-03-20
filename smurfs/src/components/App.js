cf30a9ee7a199ec3c66e8d5a1bf0881562e0a2f1
jkuenzinger <jkuenzinger228@gmail.com>
2020-03-20 08:30:30 -0700

    set up my use state and functions for the get post and put functions for the smurf endpoint

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,40 @@
-import React, { Component } from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import smurfContexts from '../contexts/smurfContext';
+import smurfList from './SmurfList';
+import smurfForm from "./SmurfForm";
+import smurf from './smurf';
 import "./App.css";
-class App extends Component {
-  render() {
+
+
+
+
+export default function App () {
+  const [smurfState, setSmurfState] = useState([]);
+    function getSmurfs(){
+      axios.get('http:// localhost:3333/smurfs', smurf)
+      .then(res => setSmurfState(res.data))
+      .catch(err => console.log('error recieving data', err))
+    }
+    
+    function postSmurfs(smurf) {
+      axios
+      .post('http:// localhost:3333/smurfs', smurf)
+      .then(res => setSmurfState(res.data))
+      .catch(err => console.log('error recieving data', err))
+    }
+    function updateSmurfs(smurf){
+      axios
+        .put('http:// localhost:3333/smurfs', smurf)
+        .then(res => setSmurftState(res.data))
+        .catch(err => console.log('error putting smurf '), err)
+    }
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+    
       </div>
     );
   }
-}
+
 
 export default App;
